47d96816b0e17f39635e7f7a2148e78f7970ce1d
khushalasawa <khushalasawa99@gmail.com>
2021-11-04 00:21:48 +0530

    Front End completed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Profiler } from "react";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,41 @@
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
+export default function profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/1.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Khushal Asawa</h4>
+              <span className="profileInfoDesc">
+                Hello I am a software engineer
+              </span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
