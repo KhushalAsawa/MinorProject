630fedc8d9ceba0575599bc13449ccc90d2b5097
khushalasawa <khushalasawa99@gmail.com>
2021-11-04 00:31:17 +0530

    changed App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Profile from "./pages/profile/Profile";
-
+// This is app.js
 function App() {
   return <Home />;
 }
